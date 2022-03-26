6ab43439eecb4281c80174d55a2fd9bea90a6929
maxim kinier <govormaxsim@gmail.com>
2022-03-26 17:48:50 +0300

    index implementation

---
+++ index.js
@@ -0,0 +1,41 @@
+const { stdin, stdout } = process;
+const util = require("util");
+const taskTransform = require("./taskTransform");
+
+const fs = require('fs');
+const stream = require("stream");
+const {program} = require("commander");
+const pipeline = util.promisify(stream.pipeline);
+
+
+const actionHandler = async () => {
+    let args = program.opts();
+
+
+
+    try {
+        // noinspection JSCheckFunctionSignatures
+        await pipeline(
+            args['input'] ? fs.createReadStream(args['input']) : stdin,
+            new taskTransform(),
+            args['output'] ? fs.createWriteStream(args['output'], {flags: "a"}) : stdout
+        );
+
+
+    } catch (e) {
+        process.stderr.write(`Определенно произошла проблема ${e.message}\n`);
+        process.exit(1);
+    }
+}
+
+
+
+process.stdin.setEncoding("utf-8");
+
+program
+    .option('-i, --input <filename>')
+    .option('-o, --output <filename>')
+    .action(actionHandler);
+program.parse();
+
+
